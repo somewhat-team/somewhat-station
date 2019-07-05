10f172061fdd75e87a44ec4f6254860ed68716b0
KosyakMakc <kosyakmakc@live.com>
2019-07-05 11:15:23 +0500

    first stage

---
+++ Assets/arrowMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowMove : MonoBehaviour
+{
+	private float speed = 2.5f;
+	public GameObject player;
+
+	void Start() {
+		
+	}
+
+	void Update()
+	{
+		float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		player.transform.Translate(x, 0, y);
+	}
+}
